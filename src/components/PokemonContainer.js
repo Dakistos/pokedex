a782252734876343a3dc360b537a98f9b49c7884
Mathias <mathias.dupont50@gmail.com>
2019-10-13 18:56:44 +0200

    Add sprites

--- src/components/PokemonContainer.js
+++ src/components/PokemonContainer.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, {Component} from 'react'
 import '../css/cell.css'
 
-const PokemonContainer = ({name}) => {
-    return (
-        <div className='cell'>
-            <p>{name}</p>
-        </div>
-    )
+class PokemonContainer extends Component {
+    state = {
+        name: '',
+        index: '',
+        img: ''
+
+    }
+
+    componentDidMount(){
+        const {name, sprites} = this.props
+
+        const index = sprites.split('/')[sprites.split('/').length - 2];
+        const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index}.png`
+        this.setState({name, img, index})
+    }
+
+    render(){
+        return (
+            <button className='cell'>
+                <p>{this.state.name}</p>
+                <img src={this.state.img}/>
+            </button>
+        )
+    }
 }
 
 export default PokemonContainer;
--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react'
-import Cell from './PokemonContainer'
+import Container from './PokemonContainer'
 import axios from 'axios'
-
-const API = 'https://pokeapi.co/api/v2/pokemon/'
 
 
 class PokemonList extends Component {
@@ -14,21 +12,27 @@ class PokemonList extends Component {
         };
     }
 
+
     async componentDidMount(){
+        const API = 'https://pokeapi.co/api/v2/pokemon?limit=100'
+
         const response = await axios.get(API);
         this.setState({pokemon: response.data['results']})
+        console.log(response.data['results'])
     }
 
     render(){
         return (
             <div>
-            <h1>Pokemon</h1>
-            <p>{this.state.pokemon.map((pokemon, i) => (
-                <Cell
-                key={i}
-                name={pokemon.name}
-                />
-            ))}</p>
+                <h1>Pokemon</h1>
+                <p>{this.state.pokemon.map((pokemon, i) => (
+                    <Container
+                    key={i}
+                    name={pokemon.name}
+                    sprites={pokemon.url}
+                    />
+                    ))}
+                </p>
             </div>
         )
     }
